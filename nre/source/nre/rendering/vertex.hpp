04d4a7f6c1633eb48647f0ace634ed9ca026b344
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-05-05 18:57:13 +0700

    Update vertex.hpp

--- nre/source/nre/rendering/vertex.hpp
+++ nre/source/nre/rendering/vertex.hpp
@@ -6,18 +6,43 @@
 
 namespace nre {
 
-	struct F_vertex {
-
-		F_vector3_f32 position;
-		F_vector3_f32 normal;
-		F_vector3_f32 tangent;
-		F_vector4_f32 uv;
-
-	};
-
 	using F_vertex_position = F_vector3_f32;
 	using F_vertex_normal = F_vector3_f32;
 	using F_vertex_tangent = F_vector3_f32;
 	using F_vertex_uv = F_vector4_f32;
+
+	inline F_input_assembler_desc default_vertex_input_assembler_desc()
+	{
+		return {
+			.vertex_attribute_groups = {
+				{
+					{ // vertex position buffer
+						{
+							.name = "POSITION",
+							.format = E_format::R32G32B32_FLOAT
+						}
+					},
+					{ // vertex normal buffer
+						{
+							.name = "NORMAL",
+							.format = E_format::R32G32B32_FLOAT
+						}
+					},
+					{ // vertex tangent buffer
+						{
+							.name = "TANGENT",
+							.format = E_format::R32G32B32_FLOAT
+						}
+					},
+					{ // vertex uv buffer
+						{
+							.name = "UV",
+							.format = E_format::R32G32B32A32_FLOAT
+						}
+					}
+				}
+			}
+		};
+	}
 
 }
