770489f8ce21a8626cea6143fe34a3d42cf2af19
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-05-06 01:50:15 +0700

    update submodules

---
+++ nre/resources/shaders/depth_stencil_view.hlsl
@@ -0,0 +1,54 @@
+
+cbuffer uniform_data : register(b0) {
+
+    float4x4 projection_matrix;
+    float4x4 object_transform;
+    float4x4 view_transform;
+
+}
+
+struct F_vertex_to_pixel {
+
+    float4 clip_position : SV_POSITION;
+    float3 world_position : POSITION;
+    float3 world_normal : NORMAL;
+    float4 uv : UV;
+
+};
+
+F_vertex_to_pixel vmain(
+    float3 local_position : POSITION, 
+    float3 local_normal : NORMAL, 
+    float4 uv : UV
+) {
+
+    float3 world_position = mul(object_transform, float4(local_position, 1.0f)).xyz;
+    float3 world_normal = normalize(mul((float3x3)object_transform, local_normal));
+    float3 view_space_position = mul(view_transform, float4(world_position, 1.0f)).xyz;
+
+    F_vertex_to_pixel output;
+    output.clip_position = mul(projection_matrix, float4(view_space_position, 1.0f));
+    output.world_position = world_position;
+    output.world_normal = world_normal;
+    output.uv = uv;
+
+    return output;
+}
+
+float4 pmain_lambert_lighting(F_vertex_to_pixel input) : SV_TARGET {
+
+    float t = dot(input.world_normal, float3(0, 1, 0));
+    return float4(1, 1, 1, 1) * lerp(0.7f, 0.15f, t * 0.5f + 0.5f);
+}
+float4 pmain_show_world_position(F_vertex_to_pixel input) : SV_TARGET {
+
+    return float4(input.world_position, 1);
+}
+float4 pmain_show_world_normal(F_vertex_to_pixel input) : SV_TARGET {
+
+    return float4(input.world_normal, 1);
+}
+float4 pmain_show_uv(F_vertex_to_pixel input) : SV_TARGET {
+
+    return input.uv;
+}
