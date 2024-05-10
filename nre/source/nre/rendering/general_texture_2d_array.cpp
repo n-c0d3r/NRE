d052a57d5381ff6c9158f4cdcdff1608f0316ab5
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-05-11 03:24:36 +0700

    add general texture 2d array

--- nre/source/nre/rendering/general_texture_2d_array.cpp
+++ nre/source/nre/rendering/general_texture_2d_array.cpp
@@ -1,1 +1,533 @@
 #include <nre/rendering/general_texture_2d_array.hpp>
+#include <nre/rendering/render_system.hpp>
+
+
+
+namespace nre {
+
+	F_general_texture_2d_array::F_general_texture_2d_array(
+		u32 width,
+		u32 height,
+		u32 array_size,
+		const F_data& data,
+		E_format format,
+		u32 mip_level_count,
+		F_sample_desc sample_desc,
+		E_resource_bind_flag bind_flags,
+		E_resource_heap_type heap_type,
+		const G_string& name
+	) :
+		builder_(width, height, array_size, (F_data&)data, format),
+		name_(name)
+	{
+		if(builder_.is_valid())
+		{
+
+			F_initial_resource_data initial_resource_data = {
+				.data_p = (void*)(data.data())
+			};
+
+			buffer_p_ = H_texture::create_2d_array(
+				NRE_RENDER_DEVICE(),
+				initial_resource_data,
+				width,
+				height,
+				array_size,
+				format,
+				mip_level_count,
+				sample_desc,
+				bind_flags,
+				heap_type
+			);
+
+			if (flag_is_has(bind_flags, E_resource_bind_flag::SRV))
+			{
+
+				srv_p_ = H_resource_view::create_srv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::UAV))
+			{
+
+				uav_p_ = H_resource_view::create_uav(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::RTV))
+			{
+
+				rtv_p_ = H_resource_view::create_rtv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::DSV))
+			{
+
+				dsv_p_ = H_resource_view::create_dsv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+		}
+	}
+	F_general_texture_2d_array::F_general_texture_2d_array(
+		u32 width,
+		u32 height,
+		u32 array_size,
+		const F_texels& texels,
+		E_format format,
+		u32 mip_level_count,
+		F_sample_desc sample_desc,
+		E_resource_bind_flag bind_flags,
+		E_resource_heap_type heap_type,
+		const G_string& name
+	) :
+		builder_(width, height, array_size, texels),
+		name_(name)
+	{
+		NCPP_ASSERT((width * height * array_size) == texels.size()) << "invalid texel count";
+
+		if(builder_.is_valid())
+		{
+			auto data = builder_.data(format);
+
+			F_initial_resource_data initial_resource_data = {
+				.data_p = (void*)(data.data())
+			};
+
+			buffer_p_ = H_texture::create_2d_array(
+				NRE_RENDER_DEVICE(),
+				initial_resource_data,
+				width,
+				height,
+				array_size,
+				format,
+				mip_level_count,
+				sample_desc,
+				bind_flags,
+				heap_type
+			);
+
+			if (flag_is_has(bind_flags, E_resource_bind_flag::SRV))
+			{
+
+				srv_p_ = H_resource_view::create_srv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::UAV))
+			{
+
+				uav_p_ = H_resource_view::create_uav(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::RTV))
+			{
+
+				rtv_p_ = H_resource_view::create_rtv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::DSV))
+			{
+
+				dsv_p_ = H_resource_view::create_dsv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+		}
+	}
+	F_general_texture_2d_array::F_general_texture_2d_array(
+		const F_texture_2d_array_builder& builder,
+		E_format format,
+		u32 mip_level_count,
+		F_sample_desc sample_desc,
+		E_resource_bind_flag bind_flags,
+		E_resource_heap_type heap_type,
+		const G_string& name
+	) :
+		builder_(builder),
+		name_(name)
+	{
+		if(builder_.is_valid())
+		{
+			auto data = builder_.data(format);
+
+			F_initial_resource_data initial_resource_data = {
+				.data_p = (void*)(data.data())
+			};
+
+			buffer_p_ = H_texture::create_2d_array(
+				NRE_RENDER_DEVICE(),
+				initial_resource_data,
+				builder_.width(),
+				builder_.height(),
+				builder_.array_size(),
+				format,
+				mip_level_count,
+				sample_desc,
+				bind_flags,
+				heap_type
+			);
+
+			if (flag_is_has(bind_flags, E_resource_bind_flag::SRV))
+			{
+
+				srv_p_ = H_resource_view::create_srv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::UAV))
+			{
+
+				uav_p_ = H_resource_view::create_uav(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::RTV))
+			{
+
+				rtv_p_ = H_resource_view::create_rtv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::DSV))
+			{
+
+				dsv_p_ = H_resource_view::create_dsv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+		}
+	}
+	F_general_texture_2d_array::F_general_texture_2d_array(
+		F_texture_2d_array_builder&& builder,
+		E_format format,
+		u32 mip_level_count,
+		F_sample_desc sample_desc,
+		E_resource_bind_flag bind_flags,
+		E_resource_heap_type heap_type,
+		const G_string& name
+	) :
+		builder_(std::move(builder)),
+		name_(name)
+	{
+		if(builder_.is_valid())
+		{
+			auto data = builder_.data(format);
+
+			F_initial_resource_data initial_resource_data = {
+				.data_p = (void*)(data.data())
+			};
+
+			buffer_p_ = H_texture::create_2d_array(
+				NRE_RENDER_DEVICE(),
+				initial_resource_data,
+				builder_.width(),
+				builder_.height(),
+				builder_.array_size(),
+				format,
+				mip_level_count,
+				sample_desc,
+				bind_flags,
+				heap_type
+			);
+
+			if (flag_is_has(bind_flags, E_resource_bind_flag::SRV))
+			{
+
+				srv_p_ = H_resource_view::create_srv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::UAV))
+			{
+
+				uav_p_ = H_resource_view::create_uav(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::RTV))
+			{
+
+				rtv_p_ = H_resource_view::create_rtv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::DSV))
+			{
+
+				dsv_p_ = H_resource_view::create_dsv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+		}
+	}
+	F_general_texture_2d_array::~F_general_texture_2d_array() {
+
+	}
+
+	void F_general_texture_2d_array::rebuild(
+		u32 width,
+		u32 height,
+		u32 array_size,
+		const F_data& data,
+		E_format format,
+		u32 mip_level_count,
+		F_sample_desc sample_desc,
+		E_resource_bind_flag bind_flags,
+		E_resource_heap_type heap_type
+	) {
+		builder_ = F_texture_2d_array_builder(
+			width,
+			height,
+			array_size,
+			(F_data&)data,
+			format
+		);
+
+		if(builder_.is_valid())
+		{
+			F_initial_resource_data initial_resource_data = {
+				.data_p = (void*)(data.data())
+			};
+
+			buffer_p_ = H_texture::create_2d_array(
+				NRE_RENDER_DEVICE(),
+				initial_resource_data,
+				width,
+				height,
+				array_size,
+				format,
+				mip_level_count,
+				sample_desc,
+				bind_flags,
+				heap_type
+			);
+
+			if (flag_is_has(bind_flags, E_resource_bind_flag::SRV))
+			{
+
+				srv_p_ = H_resource_view::create_srv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::UAV))
+			{
+
+				uav_p_ = H_resource_view::create_uav(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::RTV))
+			{
+
+				rtv_p_ = H_resource_view::create_rtv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::DSV))
+			{
+
+				dsv_p_ = H_resource_view::create_dsv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+		}
+	}
+	void F_general_texture_2d_array::rebuild(
+		u32 width,
+		u32 height,
+		u32 array_size,
+		const F_texels& texels,
+		E_format format,
+		u32 mip_level_count,
+		F_sample_desc sample_desc,
+		E_resource_bind_flag bind_flags,
+		E_resource_heap_type heap_type
+	) {
+		NCPP_ASSERT((width * height * array_size) == texels.size()) << "invalid texel count";
+
+		builder_ = F_texture_2d_array_builder(
+			width,
+			height,
+			array_size,
+			texels
+		);
+
+		if(builder_.is_valid())
+		{
+			auto data = builder_.data(format);
+
+			F_initial_resource_data initial_resource_data = {
+				.data_p = (void*)(data.data())
+			};
+
+			buffer_p_ = H_texture::create_2d_array(
+				NRE_RENDER_DEVICE(),
+				initial_resource_data,
+				width,
+				height,
+				array_size,
+				format,
+				mip_level_count,
+				sample_desc,
+				bind_flags,
+				heap_type
+			);
+
+			if (flag_is_has(bind_flags, E_resource_bind_flag::SRV))
+			{
+
+				srv_p_ = H_resource_view::create_srv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::UAV))
+			{
+
+				uav_p_ = H_resource_view::create_uav(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::RTV))
+			{
+
+				rtv_p_ = H_resource_view::create_rtv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::DSV))
+			{
+
+				dsv_p_ = H_resource_view::create_dsv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+		}
+	}
+	void F_general_texture_2d_array::rebuild(
+		const F_texture_2d_array_builder& builder,
+		E_format format,
+		u32 mip_level_count,
+		F_sample_desc sample_desc,
+		E_resource_bind_flag bind_flags,
+		E_resource_heap_type heap_type
+	) {
+		builder_ = builder;
+
+		if(builder_.is_valid())
+		{
+			auto data = builder_.data(format);
+
+			F_initial_resource_data initial_resource_data = {
+				.data_p = (void*)(data.data())
+			};
+
+			buffer_p_ = H_texture::create_2d_array(
+				NRE_RENDER_DEVICE(),
+				initial_resource_data,
+				builder_.width(),
+				builder_.height(),
+				builder_.array_size(),
+				format,
+				mip_level_count,
+				sample_desc,
+				bind_flags,
+				heap_type
+			);
+
+			if (flag_is_has(bind_flags, E_resource_bind_flag::SRV))
+			{
+
+				srv_p_ = H_resource_view::create_srv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::UAV))
+			{
+
+				uav_p_ = H_resource_view::create_uav(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::RTV))
+			{
+
+				rtv_p_ = H_resource_view::create_rtv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::DSV))
+			{
+
+				dsv_p_ = H_resource_view::create_dsv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+		}
+	}
+	void F_general_texture_2d_array::rebuild(
+		F_texture_2d_array_builder&& builder,
+		E_format format,
+		u32 mip_level_count,
+		F_sample_desc sample_desc,
+		E_resource_bind_flag bind_flags,
+		E_resource_heap_type heap_type
+	) {
+		builder_ = std::move(builder);
+
+		if(builder_.is_valid())
+		{
+			auto data = builder_.data(format);
+
+			F_initial_resource_data initial_resource_data = {
+				.data_p = (void*)(data.data())
+			};
+
+			buffer_p_ = H_texture::create_2d_array(
+				NRE_RENDER_DEVICE(),
+				initial_resource_data,
+				builder_.width(),
+				builder_.height(),
+				builder_.array_size(),
+				format,
+				mip_level_count,
+				sample_desc,
+				bind_flags,
+				heap_type
+			);
+
+			if (flag_is_has(bind_flags, E_resource_bind_flag::SRV))
+			{
+
+				srv_p_ = H_resource_view::create_srv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::UAV))
+			{
+
+				uav_p_ = H_resource_view::create_uav(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::RTV))
+			{
+
+				rtv_p_ = H_resource_view::create_rtv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+			if (flag_is_has(bind_flags, E_resource_bind_flag::DSV))
+			{
+
+				dsv_p_ = H_resource_view::create_dsv(
+					NCPP_FOH_VALID(buffer_p_)
+				);
+			}
+		}
+	}
+
+}
--- nre/source/nre/rendering/general_texture_2d_array.hpp
+++ nre/source/nre/rendering/general_texture_2d_array.hpp
@@ -1,3 +1,140 @@
 #pragma once
 
 #include <nre/prerequisites.hpp>
+#include <nre/rendering/texel.hpp>
+#include <nre/rendering/texture_2d_array_builder.hpp>
+
+
+
+namespace nre {
+
+	class NRE_API F_general_texture_2d_array {
+
+	public:
+		using F_data = TG_vector<u8>;
+
+
+
+	private:
+		U_texture_2d_array_handle buffer_p_;
+		F_texture_2d_array_builder builder_;
+		U_srv_handle srv_p_;
+		U_uav_handle uav_p_;
+		U_rtv_handle rtv_p_;
+		U_dsv_handle dsv_p_;
+		G_string name_;
+
+	public:
+		NCPP_FORCE_INLINE K_texture_2d_array_handle buffer_p() const noexcept { return buffer_p_; }
+		NCPP_FORCE_INLINE const F_texture_2d_array_builder& builder() const noexcept { return builder_; }
+		NCPP_FORCE_INLINE K_srv_handle srv_p() const noexcept { return srv_p_; }
+		NCPP_FORCE_INLINE K_uav_handle uav_p() const noexcept { return uav_p_; }
+		NCPP_FORCE_INLINE K_rtv_handle rtv_p() const noexcept { return rtv_p_; }
+		NCPP_FORCE_INLINE K_dsv_handle dsv_p() const noexcept { return dsv_p_; }
+		NCPP_FORCE_INLINE const G_string& name() const noexcept { return name_; }
+
+
+
+	public:
+		F_general_texture_2d_array(
+			u32 width,
+			u32 height,
+			u32 array_size,
+			const F_data& data = {},
+			E_format format = E_format::R8G8B8A8_UNORM,
+			u32 mip_level_count = 1,
+			F_sample_desc sample_desc = F_sample_desc{},
+			E_resource_bind_flag bind_flags = E_resource_bind_flag::SRV,
+			E_resource_heap_type heap_type = E_resource_heap_type::GREAD_GWRITE,
+			const G_string& name = ""
+		);
+		F_general_texture_2d_array(
+			u32 width,
+			u32 height,
+			u32 array_size,
+			const F_texels& texels,
+			E_format format = E_format::R8G8B8A8_UNORM,
+			u32 mip_level_count = 1,
+			F_sample_desc sample_desc = F_sample_desc{},
+			E_resource_bind_flag bind_flags = E_resource_bind_flag::SRV,
+			E_resource_heap_type heap_type = E_resource_heap_type::GREAD_GWRITE,
+			const G_string& name = ""
+		);
+		F_general_texture_2d_array(
+			const F_texture_2d_array_builder& builder,
+			E_format format = E_format::R8G8B8A8_UNORM,
+			u32 mip_level_count = 1,
+			F_sample_desc sample_desc = F_sample_desc{},
+			E_resource_bind_flag bind_flags = E_resource_bind_flag::SRV,
+			E_resource_heap_type heap_type = E_resource_heap_type::GREAD_GWRITE,
+			const G_string& name = ""
+		);
+		F_general_texture_2d_array(
+			F_texture_2d_array_builder&& builder,
+			E_format format = E_format::R8G8B8A8_UNORM,
+			u32 mip_level_count = 1,
+			F_sample_desc sample_desc = F_sample_desc{},
+			E_resource_bind_flag bind_flags = E_resource_bind_flag::SRV,
+			E_resource_heap_type heap_type = E_resource_heap_type::GREAD_GWRITE,
+			const G_string& name = ""
+		);
+		~F_general_texture_2d_array();
+
+	public:
+		NCPP_DISABLE_COPY(F_general_texture_2d_array);
+
+	public:
+		void rebuild(
+			u32 width,
+			u32 height,
+			u32 array_size,
+			const F_data& data = {},
+			E_format format = E_format::R8G8B8A8_UNORM,
+			u32 mip_level_count = 1,
+			F_sample_desc sample_desc = F_sample_desc{},
+			E_resource_bind_flag bind_flags = E_resource_bind_flag::SRV,
+			E_resource_heap_type heap_type = E_resource_heap_type::GREAD_GWRITE
+		);
+		void rebuild(
+			u32 width,
+			u32 height,
+			u32 array_size,
+			const F_texels& texels,
+			E_format format = E_format::R8G8B8A8_UNORM,
+			u32 mip_level_count = 1,
+			F_sample_desc sample_desc = F_sample_desc{},
+			E_resource_bind_flag bind_flags = E_resource_bind_flag::SRV,
+			E_resource_heap_type heap_type = E_resource_heap_type::GREAD_GWRITE
+		);
+		void rebuild(
+			const F_texture_2d_array_builder& builder,
+			E_format format = E_format::R8G8B8A8_UNORM,
+			u32 mip_level_count = 1,
+			F_sample_desc sample_desc = F_sample_desc{},
+			E_resource_bind_flag bind_flags = E_resource_bind_flag::SRV,
+			E_resource_heap_type heap_type = E_resource_heap_type::GREAD_GWRITE
+		);
+		void rebuild(
+			F_texture_2d_array_builder&& builder,
+			E_format format = E_format::R8G8B8A8_UNORM,
+			u32 mip_level_count = 1,
+			F_sample_desc sample_desc = F_sample_desc{},
+			E_resource_bind_flag bind_flags = E_resource_bind_flag::SRV,
+			E_resource_heap_type heap_type = E_resource_heap_type::GREAD_GWRITE
+		);
+		NCPP_FORCE_INLINE b8 is_texels_available() const noexcept {
+
+			return builder_.is_texels_available();
+		}
+		NCPP_FORCE_INLINE void release_texels() {
+
+			builder_.release_texels();
+		}
+		NCPP_FORCE_INLINE F_texture_2d_array_builder move_texels() {
+
+			return builder_.move_texels();
+		}
+
+	};
+
+}
