8db696894b327538dee9b9b508d2a63cea66ae71
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-08-02 19:45:40 +0700

    remove FreeImage

--- cmake/NRE/CheckDependencies.cmake
+++ cmake/NRE/CheckDependencies.cmake
@@ -82,19 +82,6 @@ if(NOT TARGET FreeImage)
     endif()
 endif()
 
-######################################################################################
-##   FreeImage checking
-######################################################################################
-if(NOT TARGET FreeImage)
-    if(NOT EXISTS "${NRE_SUBMODULES_DIR}/FreeImage")
-        if(WIN32)
-            execute_process(COMMAND "${NRE_SCRIPTS_DIR}/check_submodules.bat")
-        elseif(UNIX)
-            execute_process(COMMAND "${NRE_SCRIPTS_DIR}/check_submodules.sh")
-        endif()
-    endif()
-endif()
-
 #####################################################################################
 #   imgui checking
 #####################################################################################
