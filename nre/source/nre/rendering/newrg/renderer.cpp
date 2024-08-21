25b9631215ebd73ce6bb689f8763b162f32a9f20
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-08-21 20:28:28 +0700

    Update renderer.cpp

--- nre/source/nre/rendering/newrg/renderer.cpp
+++ nre/source/nre/rendering/newrg/renderer.cpp
@@ -24,32 +24,24 @@ namespace nre::newrg
     {
         auto render_graph_p = F_render_graph::instance_p();
 
-        // F_render_resource* render_resource_p = 0;
-        //
-        // render_resource_p = render_graph_p->create_resource(
-        //     H_resource_desc::T_create_buffer_desc<F_vector4_f32>(
-        //         128,
-        //         ED_resource_flag::INPUT_BUFFER
-        //     )
-        //     NRE_OPTIONAL_DEBUG_PARAM("demo_render_resource")
-        // );
+        F_render_resource* render_resource_p = 0;
 
-        // if(res_p_)
-        // {
-        //     render_resource_p = render_graph_p->import_resource(NCPP_FOH_VALID(res_p_));
-        // }
-        // else
-        // {
-        //     render_resource_p = render_graph_p->create_resource(
-        //         H_resource_desc::T_create_buffer_desc<F_vector4_f32>(
-        //             128,
-        //             ED_resource_flag::INPUT_BUFFER
-        //         )
-        //         NRE_OPTIONAL_DEBUG_PARAM("demo_render_resource")
-        //     );
-        // }
-        //
-        // res_p_ = render_graph_p->export_resource(render_resource_p);
+        if(res_p_)
+        {
+            render_resource_p = render_graph_p->import_resource(NCPP_FOH_VALID(res_p_));
+        }
+        else
+        {
+            render_resource_p = render_graph_p->create_resource(
+                H_resource_desc::T_create_buffer_desc<F_vector4_f32>(
+                    128,
+                    ED_resource_flag::INPUT_BUFFER
+                )
+                NRE_OPTIONAL_DEBUG_PARAM("demo_render_resource")
+            );
+        }
+
+        res_p_ = render_graph_p->export_resource(render_resource_p);
 
         {
             auto render_pass_p = render_graph_p->create_pass(
@@ -59,25 +51,25 @@ namespace nre::newrg
                 ED_pipeline_state_type::GRAPHICS
                 NRE_OPTIONAL_DEBUG_PARAM("demo_render_pass_1")
             );
-            // render_pass_p->add_resource_state({
-            //     render_resource_p,
-            //     ED_resource_flag::INPUT_BUFFER
-            // });
+            render_pass_p->add_resource_state({
+                render_resource_p,
+                ED_resource_flag::INPUT_BUFFER
+            });
         }
 
-        // {
-        //     auto render_pass_p = render_graph_p->create_pass(
-        //         [](F_render_pass* render_pass_p, TKPA_valid<A_command_list> command_list_p)
-        //         {
-        //         },
-        //         ED_pipeline_state_type::GRAPHICS
-        //         NRE_OPTIONAL_DEBUG_PARAM("demo_render_pass_2")
-        //     );
-        //     // render_pass_p->add_resource_state({
-        //     //     render_resource_p,
-        //     //     ED_resource_flag::INPUT_BUFFER
-        //     // });
-        // }
+        {
+            auto render_pass_p = render_graph_p->create_pass(
+                [](F_render_pass* render_pass_p, TKPA_valid<A_command_list> command_list_p)
+                {
+                },
+                ED_pipeline_state_type::GRAPHICS
+                NRE_OPTIONAL_DEBUG_PARAM("demo_render_pass_2")
+            );
+            render_pass_p->add_resource_state({
+                render_resource_p,
+                ED_resource_flag::INPUT_BUFFER
+            });
+        }
     }
     void F_renderer::render_frame()
     {
