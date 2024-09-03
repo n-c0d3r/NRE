09f2605426362ebdbaa6c5e5d7da17c96252c2ac
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-09-03 21:36:51 +0700

    Update bootstrap.cpp

--- nre/samples/newrg/abytek_gemetry/source/bootstrap.cpp
+++ nre/samples/newrg/abytek_gemetry/source/bootstrap.cpp
@@ -11,7 +11,7 @@ int main() {
 	auto application_p = TU<F_application>()(
 		F_application_desc {
 			.main_surface_desc = {
-				.title = L"Transient Resource Readback (NewRG)",
+				.title = L"Abytek Geometry (NewRG)",
 				.size = { 1024, 700 }
 			}
 		}
