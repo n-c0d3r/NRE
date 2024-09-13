ea6075d05ff8a418db2160bda386f46697e154b5
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-09-13 14:42:25 +0700

    Update bootstrap.cpp

--- nre/samples/newrg/build_unified_mesh/source/bootstrap.cpp
+++ nre/samples/newrg/build_unified_mesh/source/bootstrap.cpp
@@ -29,7 +29,7 @@ int main() {
 
 	auto render_path_p = TU<F_render_path>()();
 
-	auto original_mesh_asset_p = NRE_ASSET_SYSTEM()->load_asset("models/hq_cube_1.obj").T_cast<F_static_mesh_asset>();
+	auto original_mesh_asset_p = NRE_ASSET_SYSTEM()->load_asset("models/iron_man_helmet.obj").T_cast<F_static_mesh_asset>();
 	// auto original_mesh_asset_p = NRE_ASSET_SYSTEM()->load_asset("models/hq_cube.obj").T_cast<F_static_mesh_asset>();
 	// auto original_mesh_asset_p = NRE_ASSET_SYSTEM()->load_asset("models/rock.obj").T_cast<F_static_mesh_asset>();
 	auto original_mesh_p = original_mesh_asset_p->mesh_p;
