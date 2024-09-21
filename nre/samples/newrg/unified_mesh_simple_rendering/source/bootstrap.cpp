97fc21b17842eb48dfbf90c5eedb669848a7f753
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-09-21 22:30:44 +0700

    add unified mesh simple rendering sample

---
+++ nre/samples/newrg/unified_mesh_simple_rendering/source/bootstrap.cpp
@@ -0,0 +1,76 @@
+
+#include <nre/.hpp>
+
+using namespace nre;
+using namespace nre::newrg;
+
+
+
+struct F_cb_data
+{
+	F_matrix4x4_f32 local_to_world_matrix;
+	F_matrix4x4_f32 world_to_clip_matrix;
+};
+
+
+
+int main() {
+
+	auto application_p = TU<F_application>()(
+		F_application_desc {
+			.main_surface_desc = {
+				.title = L"Unified Mesh Simple Rendering (NewRG)",
+				.size = { 1024, 700 }
+			}
+		}
+	);
+
+
+
+	auto render_path_p = TU<F_render_path>()();
+
+	auto unified_mesh_asset_p = H_unified_mesh_asset::load("models/rock.obj");
+	auto unified_mesh_p = unified_mesh_asset_p->mesh_p();
+
+
+
+	// application events
+	{
+		NRE_APPLICATION_STARTUP(application_p) {
+		};
+		NRE_APPLICATION_SHUTDOWN(application_p) {
+
+			unified_mesh_asset_p.reset();
+		};
+		NRE_APPLICATION_GAMEPLAY_TICK(application_p) {
+
+			NRE_TICK_BY_DURATION(1.0f)
+			{
+				NCPP_INFO() << "application actor tick, fps: " << T_cout_value(application_p->fps());
+			};
+		};
+		NRE_APPLICATION_RENDER_TICK(application_p)
+		{
+		};
+	}
+
+	// render_foundation event
+	{
+		NRE_NEWRG_RENDER_FOUNDATION_RG_REGISTER()
+		{
+		};
+		NRE_NEWRG_RENDER_FOUNDATION_UPLOAD()
+		{
+		};
+		NRE_NEWRG_RENDER_FOUNDATION_READBACK()
+		{
+		};
+		NRE_NEWRG_RENDER_FOUNDATION_RELEASE()
+		{
+		};
+	}
+
+	application_p->start();
+
+	return 0;
+}
