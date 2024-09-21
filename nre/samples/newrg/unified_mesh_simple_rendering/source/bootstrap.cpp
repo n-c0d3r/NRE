9e4c9e174ad6957b71644d54d7cdd72725474a20
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-09-22 05:56:30 +0700

    Update bootstrap.cpp

--- nre/samples/newrg/unified_mesh_simple_rendering/source/bootstrap.cpp
+++ nre/samples/newrg/unified_mesh_simple_rendering/source/bootstrap.cpp
@@ -6,14 +6,6 @@ using namespace nre::newrg;
 
 
 
-struct F_cb_data
-{
-	F_matrix4x4_f32 local_to_world_matrix;
-	F_matrix4x4_f32 world_to_clip_matrix;
-};
-
-
-
 int main() {
 
 	auto application_p = TU<F_application>()(
@@ -34,6 +26,29 @@ int main() {
 
 
 
+	// create level
+	auto level_p = TU<F_level>()();
+
+	// create model actor
+	auto model_actor_p = level_p->T_create_actor();
+	auto model_transform_node_p = model_actor_p->template T_add_component<F_transform_node>();
+	model_transform_node_p->transform *= T_convert<F_matrix3x3, F_matrix4x4>(
+		make_scale(F_vector3::one() * 5.0f)
+	);
+
+	// create spectator
+	auto spectator_actor_p = level_p->T_create_actor();
+	auto spectator_transform_node_p = spectator_actor_p->template T_add_component<F_transform_node>();
+	auto spectator_camera_p = spectator_actor_p->template T_add_component<F_camera>();
+	auto spectator_p = spectator_actor_p->template T_add_component<F_spectator>();
+
+	spectator_p->position = F_vector3 { 0.0f, 0.0f, -10.0f };
+	spectator_p->move_speed = 4.0f;
+
+	spectator_camera_p->render_view_p().T_cast<F_scene_render_view>()->bind_output(NRE_MAIN_SWAPCHAIN());
+
+
+
 	// application events
 	{
 		NRE_APPLICATION_STARTUP(application_p) {
@@ -41,6 +56,7 @@ int main() {
 		NRE_APPLICATION_SHUTDOWN(application_p) {
 
 			unified_mesh_asset_p.reset();
+			level_p.reset();
 		};
 		NRE_APPLICATION_GAMEPLAY_TICK(application_p) {
 
@@ -58,6 +74,20 @@ int main() {
 	{
 		NRE_NEWRG_RENDER_FOUNDATION_RG_REGISTER()
 		{
+			auto render_graph_p = F_render_graph::instance_p();
+			auto uniform_transient_resource_uploader_p = F_uniform_transient_resource_uploader::instance_p();
+
+			H_scene_render_view::RG_register_all();
+
+			H_scene_render_view::for_each(
+				[&](TKPA_valid<F_scene_render_view> scene_render_view_p)
+				{
+					auto size = scene_render_view_p->size();
+
+					auto output_rtv_descriptor_handle = scene_render_view_p->output_rtv_descriptor_handle();
+					auto output_texture_2d_p = scene_render_view_p->output_texture_2d_p();
+				}
+			);
 		};
 		NRE_NEWRG_RENDER_FOUNDATION_UPLOAD()
 		{
