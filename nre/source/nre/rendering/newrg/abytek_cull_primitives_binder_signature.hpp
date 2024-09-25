df3b4eb7c5196cc5bd90bc0d1ddd723807216322
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-09-26 06:12:42 +0700

    Update abytek_cull_primitives_binder_signature.hpp

--- nre/source/nre/rendering/newrg/abytek_cull_primitives_binder_signature.hpp
+++ nre/source/nre/rendering/newrg/abytek_cull_primitives_binder_signature.hpp
@@ -21,6 +21,37 @@ namespace nre::newrg
     public:
         F_abytek_cull_primitives_binder_signature();
     };
+
+
+
+    class H_abytek_cull_primitives_binder_signature
+    {
+    public:
+        struct H_slots
+        {
+            static constexpr u32 primitive_transforms = 0;
+            static constexpr u32 primitive_last_transforms = 0;
+            static constexpr u32 primitive_mesh_ids = 0;
+            static constexpr u32 mesh_bboxes = 0;
+            static constexpr u32 cull_options = 0;
+            static constexpr u32 view_buffer = 1;
+            static constexpr u32 primitive_ids = 0;
+            static constexpr u32 visible_primitive_ids = 1;
+            static constexpr u32 external_output = 0;
+        };
+        struct H_slot_spaces
+        {
+            static constexpr u32 primitive_transforms = 0;
+            static constexpr u32 primitive_last_transforms = 1;
+            static constexpr u32 primitive_mesh_ids = 2;
+            static constexpr u32 mesh_bboxes = 3;
+            static constexpr u32 cull_options = 4;
+            static constexpr u32 view_buffer = 4;
+            static constexpr u32 primitive_ids = 4;
+            static constexpr u32 visible_primitive_ids = 4;
+            static constexpr u32 external_output = 4;
+        };
+    };
 }
 
 
