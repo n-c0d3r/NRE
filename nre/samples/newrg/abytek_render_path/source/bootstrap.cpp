e397beaabddd490a08214127a6207cd35879d67d
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-10-04 16:36:47 +0700

    fix bug (subpage offset when using simple draw with non root cluster level)

--- nre/samples/newrg/abytek_render_path/source/bootstrap.cpp
+++ nre/samples/newrg/abytek_render_path/source/bootstrap.cpp
@@ -21,7 +21,7 @@ int main() {
 
 	auto render_path_p = TU<F_abytek_render_path>()();
 	render_path_p->simple_draw_options.enable = true;
-	render_path_p->simple_draw_options.level = 0;
+	render_path_p->simple_draw_options.level = 1;
 
 
 
