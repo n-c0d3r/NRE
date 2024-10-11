2a57f9b7a1ffefd161e32b656708231e354a7afb
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-10-12 04:04:35 +0700

    Update abytek_render_path.hpp

--- nre/source/nre/rendering/newrg/abytek_render_path.hpp
+++ nre/source/nre/rendering/newrg/abytek_render_path.hpp
@@ -250,7 +250,7 @@ namespace nre::newrg
 
         struct F_lod_options
         {
-            f32 error_threshold = 3.0f;
+            f32 error_threshold = 9.0f;
             f32 task_capacity_factor = 64.0f;
         };
         F_lod_options lod_options;
